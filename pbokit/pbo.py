80ee8f9e11495994ccded6c111aaac506c515917
Superxpdude <superxpdude@gmail.com>
2023-06-19 21:34:49 -0500

    Added initial file structure

---
+++ pbokit/pbo.py
@@ -0,0 +1,33 @@
+# PBOKit
+# Copyright 2023 Superxpdude
+# PBO File Format
+# https://community.bistudio.com/wiki/PBO_File_Format
+import functools
+import io
+
+__all__ = [
+	"read_asciiz",
+]
+
+
+def read_asciiz(stream: io.BufferedIOBase) -> str | None:
+	"""Reads an ASCIIZ string from a buffered IO stream
+
+	Parameters
+	----------
+	stream : io.BufferedIOBase
+		File as bytes, or in-memory bytes buffer
+
+	Returns
+	-------
+	str | None
+		Decoded ASCIIZ string
+	"""
+	byteStr = b""
+	for block in iter(functools.partial(stream.read, 1), b""):
+		if block == b"\x00":
+			break
+		else:
+			byteStr += block
+
+	return byteStr.decode()
---
+++ tests/test_decode.py
@@ -0,0 +1,20 @@
+import io
+import pbokit
+import pytest
+
+
+@pytest.mark.parametrize(
+	"b, s", [
+		pytest.param(b"", "", id="Empty"),
+		pytest.param(b"Arma", "Arma", id="Basic 1"),
+		pytest.param(b"Altis", "Altis", id="Basic 2"),
+		pytest.param(b"Stratis", "Stratis", id="Basic 3"),
+		pytest.param(b"Malden\x001234", "Malden", id="ASCIIZ 1"),
+		pytest.param(
+			b"Livonia\x001234\x005678\x009999", "Livonia", id="ASCIIZ 2"
+		),
+	]
+)
+def test_decode_asciiz(b: bytes, s: str):
+	byteStream = io.BytesIO(b)
+	assert pbokit.read_asciiz(byteStream) == s
